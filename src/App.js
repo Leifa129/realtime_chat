41a3ce2afab2974e1cb6b3ca71b201f507449416
Leifa129 <leif_xx@hotmail.com>
2020-10-19 10:57:42 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Auth from './Auth';
+import firebase from 'firebase';
+import Quotes from './Quotes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+let config = {
+    apiKey: 'AIzaSyDdhmbh6aRS2DY8vVUX25HJ0CwvsJuJhI8',
+    authDomain: 'spa-auth-f0409.firebaseapp.com',
+    database: 'https://spa-auth-f0409.firebaseio.com/',
+    projectId: 'spa-auth-f0409'
+};
+
+firebase.initializeApp(config);
+
+
+
+class App extends Component
+{
+    state = {loggedIn: false};
+    authentication = firebase.auth();
+    // db = firebase.firestore();
+    authFlag = true;
+
+componentDidMount() {
+    this.authentication.onAuthStateChanged(
+        (user) => {
+            if(this.authFlag){
+                // Code here is only called once...
+                console.log('First time init call');
+                this.authFlag = false;
+            }
+            if (user != null)
+                this.setState(prevState => {
+                    return {
+                        ...prevState,
+                        loggedIn: !prevState.loggedIn
+                    };
+                });
+            if(user == null) {
+                console.log('user signed out');
+                this.setState({loggedIn: false});
+            }
+        }
+
+    )
+};
+
+
+    render()
+    {
+        return (
+            <div className="App">
+
+                {
+                    // wacky logic.. could for sure have done something cleaner.
+                    !this.authFlag ? (
+                    (!this.state.loggedIn) ?
+                    <Auth onAuth={(username, password) => {
+                    this.authentication.signInWithEmailAndPassword(username, password).then(
+                        response => {
+                            //this.setState({user: response.user});
+                            //console.log(response);
+                        }
+                    );
+
+                }} onCreateUser={(username, password) => {
+                    this.authentication.createUserWithEmailAndPassword(username, password).then(
+                        response => {
+                            // this.setState({user: response.user});
+                            //console.log(response);
+                        }
+                    )
+                }}></Auth>
+                    :
+                    <div>
+                        { <button onClick={() => {this.authentication.signOut().then()}}>Logout</button>
+                        }
+                        <Quotes />
+                    </div> ) : <div></div>
+                }
+            </div>
+        );
+    }
 }
-
 export default App;
---
+++ src/Auth.js
@@ -0,0 +1,82 @@
+import React, {Component} from 'react';
+import './Auth.css';
+
+
+
+class Auth extends Component {
+    state = {email: '', password: '', newEmail: '', newPassword: '', loggedIn: false, name:''};
+
+    signUpHandler = () => {
+        const container = document.getElementById('container');
+        container.classList.add('right-panel-active');
+};
+
+    signInHandler = () => {
+        const container = document.getElementById('container');
+        container.classList.remove('right-panel-active');
+    };
+
+
+
+
+    handleLogin = event => {
+        event.preventDefault();
+        this.props.onAuth(this.state.email, this.state.password);
+
+    };
+
+    createUser = event => {
+        event.preventDefault();
+        this.props.onCreateUser(this.state.newEmail, this.state.newPassword);
+    };
+
+    updateField = event => {
+        const data = {};
+        data[event.target.name] = event.target.value;
+        this.setState(data);
+    };
+
+
+    render() {
+        return (
+            <div className="container" id="container">
+                <div className="form-container sign-up-container">
+                    <form action="#">
+                        <h1>Create Account</h1>
+                        <input type="text" name='name' placeholder="Name" onChange={this.updateField} value={this.state.name} />
+                        <input type="email" name='newEmail' placeholder="Email" onChange={this.updateField} value={this.state.newEmail} />
+                        <input type="password" name='newPassword' placeholder="Password" onChange={this.updateField} value={this.state.newPassword} />
+                        <button onClick={this.createUser}>Sign Up</button>
+                    </form>
+                </div>
+                <div className="form-container sign-in-container">
+                    <form action="#">
+                        <h1>Sign in</h1>
+                        <input type="email" name="email" onChange={this.updateField} value={this.state.email} placeholder="Email"/>
+                        <input type="password" name="password" onChange={this.updateField} value={this.state.password} placeholder="Password"/>
+                        <a href="#">Forgot your password?</a>
+                        <button onClick={this.handleLogin}>Sign In</button>
+                    </form>
+                </div>
+                <div className="overlay-container">
+                    <div className="overlay">
+                        <div className="overlay-panel overlay-left">
+                            <h1>Welcome!</h1>
+                            <p>Sign in to view super secret information</p>
+                            <button className="ghost" onClick={this.signInHandler} id="signIn">Sign In</button>
+                        </div>
+                        <div className="overlay-panel overlay-right">
+                            <h1>New?</h1>
+                            <p>Create account</p>
+                            <button className="ghost" onClick={this.signUpHandler} id="signUp">Sign Up</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+
+export default Auth;
